e0fb9999abbdb069512b54d1fab4c28042795412
Ian Mihura <mihura.ian@gmail.com>
2020-06-08 14:38:55 +0200

    non-dh server

--- dh/dh-main.js
+++ dh/dh-main.js
@@ -44,3 +44,48 @@ let expmod = function (base, exp, mod) {
         return Math.pow(expmod(base, (exp / 2), mod), 2) % mod;
     return (base * expmod(base, (exp - 1), mod)) % mod;
 };
+
+/*** Calculator ***/
+let ShowCalculator = function () {
+    getById("calculator-segment").classList.toggle("hidden");
+};
+
+let CalcLog = function (log) {
+    getById("calc-log").innerHTML += `<p>${log}</p>`;
+};
+
+let CalculatorOTP = function () {
+    let payload = getById("calc-otp-payload").value;
+    let key = getById("calc-otp-key").value;
+    let cipher = _otp(payload, key);
+
+    CalcLog(`(OTP) ${cipher} = ${payload} xor ${key}|extended`);
+};
+
+let CalculatorExponent = function () {
+    let base = getById("calc-exp-base").value;
+    let exp = getById("calc-exp-exp").value;
+    let mod = getById("calc-exp-mod").value;
+    let result = expmod(base, exp, mod);
+
+    CalcLog(`(EXP-MOD) ${result} = ${base} ^ ${exp} mod ${mod}`);
+};
+
+let CalculatorRandom = function () {
+    const max = getById("calc-rand-max").value;
+    const random = GetRandomInteger(max);
+
+    CalcLog(`(RAND) ${random} : pseudo-random integer between ${max} & 0`);
+};
+
+/*** Server Logs ***/
+let ClearLogs = function (id) {
+    getById(id).innerHTML = "";
+};
+
+let Log = function (log) {
+    console.log(log);
+    const date = new Date();
+    const stringDate = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}:${date.getMilliseconds()}`;
+    getById("log").innerHTML += `<p>${stringDate}: ${log}</p>`;
+};
